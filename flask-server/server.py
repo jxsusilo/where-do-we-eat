b3481fe500153b367f6d1088794997a0f5a2c6a4
kaushalseelam <kaushal.seelam@gmail.com>
2023-05-27 09:59:57 -0700

    Update server.py
    adding other test routes to server.py

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/')
+def index():
+    return jsonify({'message': 'Hey, everything works!!'})
+
+
 @app.route("/members")
 def members():
-    return {"members": ["Member1", "Member2", "Member3"]}
+    return jsonify({"members": ["Member1", "Member2", "Member3"]})
 
 if __name__ == "__main__":
     app.run(debug=True)
