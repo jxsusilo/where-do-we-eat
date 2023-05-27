aec0c9ea6378eff67558031fa6376a91670f4002
Jessica Susilo <jxsusilo@gmail.com>
2023-05-26 23:47:27 -0700

    Make frontend work

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
 function App() {
+  const [data, setData] = useState([{}])
+
+  useEffect (() => {
+    fetch("http://localhost:5000/members").then(
+      res => res.json()
+    ).then(
+      data => {
+        setData(data)
+        console.log(data)
+      }
+    )
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      data
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
