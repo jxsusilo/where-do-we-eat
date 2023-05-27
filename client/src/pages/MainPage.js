4c4ba3ab8ad2917d8f54a1b2a56607b0b2bdb474
Jessica Susilo <jxsusilo@gmail.com>
2023-05-27 11:46:33 -0700

    Added login page, moved main page and login to separate pages

---
+++ client/src/pages/MainPage.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react'
+import Title from '../components/Title'
+import UserInput from '../components/UserInput'
+import '../App.css';
+import RestaurantList from '../components/RestaurantList';
+
+function MainPage() {
+  // const [data, setData] = useState([{}])
+
+  // useEffect (() => {
+  //   fetch("http://localhost:5000/members").then(
+  //     res => res.json()
+  //   ).then(
+  //     data => {
+  //       setData(data)
+  //       console.log(data)
+  //     }
+  //   )
+  // }, [])
+
+  const [checkedList, setCheckedList] = useState([]);
+
+  const cuisineList = [
+    { id: "1", value: "American" },
+    { id: "2", value: "Chinese" },
+    { id: "3", value: "Greek" },
+    { id: "4", value: "Indian" },
+    { id: "5", value: "Italian" },
+    { id: "6", value: "Japanese" },
+    { id: "7", value: "Korean" },
+    { id: "8", value: "Mexican" },
+    { id: "9", value: "Nigerian" },
+    { id: "10", value: "Thai" },
+    { id: "11", value: "Vietnamese" },
+  ];
+
+  const priceList = [
+    { id: "1", value: "$" },
+    { id: "2", value: "$$" },
+    { id: "3", value: "$$$" },
+    { id: "4", value: "$$$$" },
+  ];
+
+  const restaurants = [
+    { name: "Restaurant ABC", rating: 5, address: "123 First St, Los Angeles"},
+    { name: "Second Restaurant", rating: 3, address: "222 Second St, Irvine"},
+  ]
+
+  const sessionCode = "ABCDE1"
+
+  useEffect(() => {
+    console.log(checkedList)
+  }, [checkedList])
+
+  const submit = () => {
+    console.log('hgtuyvj');
+    //fetch('server,com/uploasd/'+ sessionCode), {body: JSON.stringify({chexcked: checkedList})}.gthem()
+  }
+
+  return (
+    <div className='App'>
+      <p>Session Code: {sessionCode}</p>
+      <div className='container'>
+        <div className='column'>
+          <UserInput name='Cuisine' listData={cuisineList} checkedList={checkedList} setCheckedList={setCheckedList} />
+          {/*<UserInput name='Price' listData={priceList} checkedList={checkedList} setCheckedList={setCheckedList} />*/}
+          <button onClick={submit}>Submit Preferences</button>
+        </div>
+        <div className='column'>
+          <h2>Results</h2>
+          <RestaurantList listData={restaurants}/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MainPage;
