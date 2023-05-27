c6ff92ea0929fd47a76a8dc7ed49c49da333cb08
Jessica Susilo <jxsusilo@gmail.com>
2023-05-27 10:31:24 -0700

    Updated cuisine list

--- client/src/App.js
+++ client/src/App.js
@@ -23,12 +23,15 @@ function App() {
   const cuisineList = [
     { id: "1", value: "American" },
     { id: "2", value: "Chinese" },
-    { id: "3", value: "Italian" },
-    { id: "4", value: "Japanese" },
-    { id: "5", value: "Korean" },
-    { id: "6", value: "Mexican" },
-    { id: "7", value: "Thai" },
-    { id: "8", value: "Vietnamese" },
+    { id: "3", value: "Greek" },
+    { id: "4", value: "Indian" },
+    { id: "5", value: "Italian" },
+    { id: "6", value: "Japanese" },
+    { id: "7", value: "Korean" },
+    { id: "8", value: "Mexican" },
+    { id: "9", value: "Nigerian" },
+    { id: "10", value: "Thai" },
+    { id: "11", value: "Vietnamese" },
   ];
 
   const priceList = [
